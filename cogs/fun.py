2d60ded159c0bc0b5e7fd1b2882548af8b6f18b2
RickRollMaster101 <82796006+RickRollMaster101@users.noreply.github.com>
2021-08-08 14:37:23 +0530

    Added sus and webshot

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,14 @@ import discord
 from discord.ext import commands
 import aiohttp
 import random2
+import requests
+import json
+with open("./config.json") as f:
+    configData = json.load(f)
+
+access_key = configData["apiflashkey"]
+
+
 
 class fun(commands.Cog):
     def __init__(self, bot):
@@ -77,6 +85,29 @@ class fun(commands.Cog):
         await ctx.send(f'{member.mention} has been beaned! For: {reason}')
         print(f'{ctx.message.author} has beaned {member} for {reason}!')
 
+    @commands.command(brief='when the impostor is sus', description='when the impostor is sus 😳')
+    async def sus(self, ctx, member: discord.Member):
+        await ctx.reply(f'https://cool-api.xyz/sus?impostor={member.avatar_url}&crewmate={ctx.author.avatar_url}')
+        print(f'{member} is sus!!!!!!!')
+
+    @commands.command(brief='spongebob')
+    async def spongebob(self, ctx):
+        await ctx.reply('watch till the end')
+        await ctx.send('https://tenor.com/view/rickroll-rick-roll-rickastley-spongebob-gif-19367765')
+
+    @commands.command()
+    async def webshot(self, ctx, website):
+        async with ctx.typing():
+            print('Download Starting...')
+
+            url = f'https://api.apiflash.com/v1/urltoimage?access_key={access_key}&url={website}'
+            req = requests.get(url)
+
+            with open('./webshots/screenshot.jpg', 'wb') as f:
+                f.write(req.content)
+            print("Download Completed!")
+            await ctx.reply(file=discord.File(r'./webshots/screenshot.jpg'))
+            print('Image sent')
 
 def setup(bot):
     bot.add_cog(fun(bot))
